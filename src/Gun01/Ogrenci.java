27fea9e657ab3cf20e12be6f64a59987ab9ee3d4
corejohn <schreibankorcan@yahoo.de>
2023-01-25 21:56:09 +0300

    üçüncü görevimi yaptım

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -3,5 +3,6 @@ package Gun01;
 public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("welcome to Github");
     }
 }
